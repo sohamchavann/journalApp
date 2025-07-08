8bcdc2d520207f051646e8838d22e3b9439abf1b
Soham Chavan <sohamchavan0629@gmail.com>
2025-07-08 12:07:16 +0530

    Lombok Integration

--- src/main/java/net/engineeringdigest/journalApp/entity/JournalEntry.java
+++ src/main/java/net/engineeringdigest/journalApp/entity/JournalEntry.java
@@ -1,14 +1,12 @@
 package net.engineeringdigest.journalApp.entity;
 
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
+
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
-import java.util.Date;
 
 @Document(collection = "journal_entries")
 
